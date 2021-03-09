891e51b7921f19ed40c4ae675c436a2ee06d252b
JBlk99 <joshualeebernard@gmail.com>
2021-03-09 19:55:19 +0000

    Moved all js associated with the map to maps.js. Fixed key icons still showing as active when active div is changed.

--- assets/scripts/maps.js
+++ assets/scripts/maps.js
@@ -119,7 +119,116 @@ function initMap() {
     }
 
 
+    function key() {
+        if ($("#location-button1").hasClass("key-icon-active") && $("#time-button1").hasClass("key-icon-active")) {
+            $(".suggestions").hide();
+            $("#c-center-day").show();
+            clearMarkers();
+            $(".result-icon").removeClass("key-icon-active");
+        } else if ($("#location-button1").hasClass("key-icon-active") && $("#time-button2").hasClass("key-icon-active")) {
+            $(".suggestions").hide();
+            $("#c-center-night").show();
+            clearMarkers();
+            $(".result-icon").removeClass("key-icon-active");
+        } else if ($("#location-button2").hasClass("key-icon-active") && $("#time-button1").hasClass("key-icon-active")) {
+            $(".suggestions").hide();
+            $("#c-bay-day").show();
+            clearMarkers();
+            $(".result-icon").removeClass("key-icon-active");
+        } else if ($("#location-button2").hasClass("key-icon-active") && $("#time-button2").hasClass("key-icon-active")) {
+            $(".suggestions").hide();
+            $("#c-bay-night").show();
+            clearMarkers();
+            $(".result-icon").removeClass("key-icon-active");
+        }
+    }
 
+    // ----------Adding  function to buttons to show which is active 
+
+    // Location buttons
+    $("#location-button1").click(function () {
+        $(this).addClass("key-icon-active");
+        $("#location-button2").removeClass("key-icon-active");
+        key();
+    });
+
+    $("#location-button2").click(function () {
+        $(this).addClass("key-icon-active");
+        $("#location-button1").removeClass("key-icon-active");
+        key();
+    });
+    // Time buttons
+    $("#time-button1").click(function () {
+        $(this).addClass("key-icon-active");
+        $("#time-button2").removeClass("key-icon-active");
+        key();
+    });
+
+    $("#time-button2").click(function () {
+        $(this).addClass("key-icon-active");
+        $("#time-button1").removeClass("key-icon-active");
+        key();
+    });
+
+    // Key buttons for Cardiff Center Day
+
+    $("#key-button1").click(function () {
+        $(this).addClass("key-icon-active");
+        $("#key-button2").removeClass("key-icon-active");
+        $("#key-button3").removeClass("key-icon-active");
+    });
+
+    $("#key-button2").click(function () {
+        $(this).addClass("key-icon-active");
+        $("#key-button1").removeClass("key-icon-active");
+        $("#key-button3").removeClass("key-icon-active");
+    });
+
+    $("#key-button3").click(function () {
+        $(this).addClass("key-icon-active");
+        $("#key-button1").removeClass("key-icon-active");
+        $("#key-button2").removeClass("key-icon-active");
+    });
+
+    // Key buttons for Cardiff Center Night
+
+    $("#key-button4").click(function () {
+        $(this).addClass("key-icon-active");
+        $("#key-button5").removeClass("key-icon-active");
+
+    });
+
+    $("#key-button5").click(function () {
+        $(this).addClass("key-icon-active");
+        $("#key-button4").removeClass("key-icon-active");
+
+    });
+
+    // Key buttons for Cardiff Bay Day
+
+    $("#key-button6").click(function () {
+        $(this).addClass("key-icon-active");
+        $("#key-button7").removeClass("key-icon-active");
+
+    });
+
+    $("#key-button7").click(function () {
+        $(this).addClass("key-icon-active");
+        $("#key-button6").removeClass("key-icon-active");
+
+    });
+    // Key buttons for Cardiff Bay Night
+    $("#key-button8").click(function () {
+        $(this).addClass("key-icon-active");
+        $("#key-button9").removeClass("key-icon-active");
+
+    });
+
+    $("#key-button9").click(function () {
+        $(this).addClass("key-icon-active");
+        $("#key-button8").removeClass("key-icon-active");
+
+    });
 
     // Map Markers for Cardiff Central night Restarents:
     var cCenterNightRestarants = [
