4e37db6615e12bd6acbd98bb36dc4e46d16fd4c5
JBlk99 <joshualeebernard@gmail.com>
2021-03-12 14:33:06 +0000

    added the JS for light/dark theme toggler

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -17,17 +17,17 @@ function closeNav() {
 // -------------------- Credit Key: 2. Gallery Start --------------------
 var slideIndex = 1;
 showSlides(slideIndex);
- 
+
 // Next/previous controls
 function plusSlides(n) {
     showSlides(slideIndex += n);
 }
- 
+
 // Thumbnail image controls
 function currentSlide(n) {
     showSlides(slideIndex = n);
 }
- 
+
 function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("mySlides");
@@ -44,3 +44,15 @@ function showSlides(n) {
     dots[slideIndex - 1].className += " active";
 }
 // -------------------- Gallery Credit Key: 2. End --------------------
+//-------------------- Light/ Dark mode --------------------
+
+// Listen for a click on the button
+$(".btn-theme-toggle").click(function () { // When btn with this class is clicked
+    
+    if ($("#theme-link").attr("href") == "assets/css/light-theme.css") {// If the current URL contains the light theme
+        // ... then switch it to "dark-theme.css"
+        $("#theme-link").attr("href", "assets/css/dark-theme.css") // It will procede to change theme to dark theme
+    } else {
+        $("#theme-link").attr("href", "assets/css/light-theme.css") //Otherwise it will change to light theme
+    }
+});
