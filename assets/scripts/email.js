66c159a3437d26e3c8cbff5378e42782a167ef21
JBlk99 <joshualeebernard@gmail.com>
2021-03-18 12:45:00 +0000

    Added Content to #contact section (HTML/CSS/JS)

---
+++ assets/scripts/email.js
@@ -0,0 +1,16 @@
+function sendMail(contactForm) {
+    emailjs.send("key-to-cardiff", "key-to-cardiff", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.email.value,
+        "subject": contactForm.subject.value,
+        "message": contactForm.message.value
+    })
+        .then(
+            function (response) {
+                console.log("SUCCESS", response);
+            },
+            function (error) {
+                console.log("FAILED", error);
+            });
+    return false;
+}
