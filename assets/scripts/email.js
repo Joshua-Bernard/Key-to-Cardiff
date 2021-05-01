991e9414737e19f15445204f9d32ec210d2ad90f
JBlk99 <joshualeebernard@gmail.com>
2021-05-01 10:53:00 +0000

    Added feedback to the form's submit button to make it clear if a submit is successful

--- assets/scripts/email.js
+++ assets/scripts/email.js
@@ -1,3 +1,6 @@
+$("#unsuccessful-msg").hide();
+$("#successful-msg").hide();
+
 function sendMail(contactForm) {
     emailjs.send("gmail", "key-to-cardiff", {
         "from_name": contactForm.name.value,
@@ -7,10 +10,26 @@ function sendMail(contactForm) {
     })
         .then(
             function (response) {
-                console.log("SUCCESS", response);
+                console.log("Submittion successful.", response);
+                $("#form-submit").hide(); // the submit button is hidden
+                $("#successful-msg").show();
+                function toggleDiv() {
+                    // set timeout code was found at www.w3schools.com/jsref/met_win_settimeout.asp
+                    setTimeout(function () {
+                        $("#successful-msg").hide();
+                    }, 5000);
+                }
             },
             function (error) {
-                console.log("FAILED", error);
+                console.log("Submittion unsuccessful", error);
+                $("#form-submit").hide(); // the submit button is hidden
+                $("#unsuccessful-msg").show();
+                function toggleDiv() {
+                    // set timeout code was found at www.w3schools.com/jsref/met_win_settimeout.asp
+                    setTimeout(function () {
+                        $("#unsuccessful-msg").hide();
+                    }, 5000);
+                }
             });
     return false;
 }
